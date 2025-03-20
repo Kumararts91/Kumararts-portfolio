5e6d520c343e681c2db8e1b50da0b00f265dcb72
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-20 10:08:52 +0530

    Description updated

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "KumarArts91",
-  description: "",
+  description: "At Kumararts91, our creative team offers exceptional branding, graphic design, digital art, web design, and multimedia solutions, turning your vision into innovative, customized designs that leave a lasting impression.",
 };
 
 export default function RootLayout({ children }) {
